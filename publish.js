dfd58e3e772f76d274660d1081fc628f63808423
levi <levi2p@gmail.com>
2018-11-02 19:06:46 +0200

    Fixed the publish script not publishing the README.

--- publish.js
+++ publish.js
@@ -1,29 +1,45 @@
 'use strict';
 
 const path = require('path');
-const fs = require('fs');
+const fsp = require('fs').promises;
 const child_process = require('child_process');
-const pkg = require('./package.json');
 
-const distDir = path.join(__dirname, './dist/');
+const projectRoot = __dirname;
+const distDir = path.join(projectRoot, './dist/');
 
-// Remove `private`
-delete pkg.private;
+main();
 
-// No longer needed
-delete pkg.scripts;
-delete pkg.devDependencies;
+async function main() {
+	const pkg = Object.assign({}, require('./package.json'));
 
-fs.writeFileSync(
-		path.join(distDir, './package.json'),
-		JSON.stringify(pkg, null, 2),
-);
+	console.log('PUBLISHING VERSION:', pkg.version);
 
-const process = child_process.spawn('npm', ['publish'], {
-	cwd: distDir,
-	stdio: 'inherit',
-});
+	// Remove `private`
+	delete pkg.private;
 
-process.on('close', () => {
-	console.log('Publish script finished.');
-});
+	// No longer needed
+	delete pkg.scripts;
+	delete pkg.devDependencies;
+
+	await Promise.all([
+		// Write fixed package.json
+		fsp.writeFile(
+				path.join(distDir, './package.json'),
+				JSON.stringify(pkg, null, 2),
+		),
+		// Copy README.md
+		fsp.copyFile(
+				path.join(projectRoot, './README.md'),
+				path.join(distDir, './README.md'),
+		),
+	]);
+
+	const process = child_process.spawn('npm', ['publish'], {
+		cwd: distDir,
+		stdio: 'inherit',
+	});
+
+	process.on('close', () => {
+		console.log('Publish script finished.');
+	});
+}
